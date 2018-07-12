9a323804097b69d32fffeb2976c2b1f9d21a3dc4
Sandeep <gurusandeepraj@gmail.com>
2018-07-13 04:07:23 +0530

    all for loo variable are declared and intialized within for loop and used arrow function without return

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,10 +1,6 @@
 // The intent of this file is to reverse
 // the string using built in functions.
 
-let res = '';
-const reverse = (str) => {
-  res = str.split('').reverse().join('');
-  return res;
-};
+const reverse = str => (str.split('').reverse().join(''));
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,8 +1,7 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
 const reverse = (str) => {
-  let i;
   let temp = '';
-  for (i = str.length - 1; i >= 0; i -= 1) {
+  for (let i = str.length - 1; i >= 0; i -= 1) {
     temp += str[i];
   }
   return (temp);
